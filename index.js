7f08b156cd088aec231737ecce70f27d08244f90
Andrew Lawson <andrew@bravenewtalent.com>
2012-03-23 16:24:44 +0000

    Added static path rather than file

--- index.js
+++ index.js
@@ -3,18 +3,31 @@
  * Serve the static js
  * @type {void}
  */
-var http = require('http'),
-    fs = require('fs');
+var http = require('http');
+var path = require('path');
+var fs = require('fs');
+ 
 http.createServer(function (request, response) {
      
-    fs.readFile('./node_modules/socket.io/lib/socket.io.js', function(error, content) {
-        if (error) {
-            response.writeHead(500);
-            response.end();
+    var filePath = './node_modules/socket.io/lib' + request.url;
+     
+    path.exists(filePath, function(exists) {
+     
+        if (exists) {
+            fs.readFile(filePath, function(error, content) {
+                if (error) {
+                    response.writeHead(500);
+                    response.end();
+                }
+                else {
+                    response.writeHead(200, { 'Content-Type': 'application/javascript' });
+                    response.end(content, 'utf-8');
+                }
+            });
         }
         else {
-            response.writeHead(200, { 'Content-Type': 'text/html' });
-            response.end(content, 'utf-8');
+            response.writeHead(404);
+            response.end();
         }
     });
      
