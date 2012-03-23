4ca596507b98d25d37bcfaa79e365b077630a3e0
Andrew Lawson <andrew@bravenewtalent.com>
2012-03-23 12:36:40 +0000

    Added remote IP to client connection log

--- index.js
+++ index.js
@@ -7,8 +7,8 @@ var Server = require('./lib/server');
 /**
  * Create the server instance
  */
-new Server(6789).on('open', function() {
+new Server(6789).on('open', function(socket) {
 
-    console.log('Client connection created');
+    console.log('Client connection created from address ' + socket.remoteAddress);
 
 });
